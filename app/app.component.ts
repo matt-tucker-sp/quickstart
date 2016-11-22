6eac0d23a05298e348fca690a6d287cd97b10f46
Matt Tucker <matt.tucker@sailpoint.com>
2016-11-21 17:00:52 -0800

    use string-utils

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { StringUtilsService } from 'string-utils';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `<h1>{{ title }}</h1>`
 })
-export class AppComponent { }
+export class AppComponent {
+    private stringUtils : StringUtilsService;
+    public title : string;
+
+    constructor(@Inject(StringUtilsService) stringUtils : StringUtilsService) {
+
+        let spaceString = 'This String has some SPACES';
+
+        this.stringUtils = stringUtils;
+
+        this.title = `"${spaceString}" is now magically  "${this.stringUtils.convertToUnderscoreCase(spaceString)}"`;
+    }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StringUtilsModule } from 'string-utils';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, StringUtilsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
